c45a21e12c886c95cddb592bb704426601a226a3
Rihards <38590234+Rihards12@users.noreply.github.com>
2020-07-14 16:53:24 +0300

    Create have installation vue.js

---
+++ have installation vue.js
@@ -0,0 +1,12 @@
+$ npm install -g vue-cli
+# create a new project using the "webpack" boilerplate
+$ vue init webpack my-project
+# install dependencies and go!
+$ cd my-project
+$ npm install
+$ npm run dev
+
+git clone https://github.com/vuejs/vue.git node_modules/vue
+cd node_modules/vue
+npm install
+npm run build
